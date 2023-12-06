3a14b1dc012e5db93d127b5c34ba4a19158962e4
jjsh03 <139740067+jjsh03@users.noreply.github.com>
2023-12-06 19:10:01 +0900

    feat: 디자인 아트워크 모달 적용

--- portfolio/src/components/designCard/DesignCard.js
+++ portfolio/src/components/designCard/DesignCard.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './style.scss';
 
 export default function DesignCard({ data }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className="designCard">
-      <div>
+      <div onClick={openModal}>
         <div className="cardHover">
           <p>{data.type}</p>
         </div>
         <img src={data.img} alt="cardImg" className="cardImg" />
       </div>
+
       <div className="cardText">
         <p>{data.name}</p>
         <p>{data.type}</p>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Webview Modal"
+        className={{
+          base: 'modal-enter-animation',
+          afterOpen: 'modal-entered',
+          beforeClose: '',
+        }}
+        overlayClassName="overlay"
+      >
+        <main className="modalMain">
+          <img
+            src={data.modal}
+            width="100%"
+            height="100%"
+            className="modalImg"
+          />
+          <button className="modalCloseButton" onClick={closeModal}>
+            CLOSE
+          </button>
+        </main>
+      </Modal>
     </div>
   );
 }
