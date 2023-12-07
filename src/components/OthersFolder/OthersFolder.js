106b0b84f8c4dea257d06e8442416c9e0ecd3946
jjsh03 <aberrant03@gmail.com>
2023-12-08 03:28:51 +0900

    feat: others 데이터 추가

--- src/components/OthersFolder/OthersFolder.js
+++ src/components/OthersFolder/OthersFolder.js
@@ -4,26 +4,23 @@ import './style.scss';
 export default function OthersFolder({ data }) {
   return (
     <div>
-      <div className={`otherFolder ${data.name}`}>
-        <div className={`folderBack ${data.name}`}>
-          <div />
-          <div />
+      <a href={data.url} target='blank' style={{ color: 'white' }}>
+        <div className={`otherFolder ${data.name}`}>
+          <div className={`folderBack ${data.name}`}>
+            <div />
+            <div />
+          </div>
+          <img src={data.img} alt='thumbnail' className='folderThumbnail'></img>
+          <div className={`folderFront ${data.name}front`}></div>
         </div>
-        <img src={data.img} alt="thumbnail" className="folderThumbnail"></img>
-        <div className={`folderFront ${data.name}front`}>
-          {/* <p>
+        <div className='folderInfo'>
+          {' '}
+          <p>
             {data.name}_{data.year}
           </p>
-          <p>{data.type}</p> */}
+          <p>{data.type}</p>
         </div>
-      </div>
-      <div className="folderInfo">
-        {' '}
-        <p>
-          {data.name}_{data.year}
-        </p>
-        <p>{data.type}</p>
-      </div>
+      </a>
     </div>
   );
 }
--- src/components/WorkComponent/OthersCompo.js
+++ src/components/WorkComponent/OthersCompo.js
@@ -4,6 +4,8 @@ import OthersFolder from '../OthersFolder/OthersFolder';
 import other1 from '../../asset/others1.png';
 import other2 from '../../asset/others2.png';
 import other3 from '../../asset/others3.png';
+import other4 from '../../asset/others4.jpg';
+import other5 from '../../asset/others5.jpg';
 
 export default function OthersCompo() {
   const otherWorks = [
@@ -13,6 +15,7 @@ export default function OthersCompo() {
       year: 2022,
       type: 'POPUP STORE',
       img: other1,
+      url: 'https://sage-h.notion.site/_-ac404ad9611342c09f2adbcbd0a50191',
     },
     {
       id: 2,
@@ -20,38 +23,35 @@ export default function OthersCompo() {
       year: 2021,
       type: 'VIDEO',
       img: other2,
+      url: 'https://youtu.be/23kFUYgp20c',
     },
     {
       id: 3,
       name: '칠성제로',
-      year: 2022,
+      year: 2021,
       type: 'IMCPLAN',
       img: other3,
+      url: 'https://sage-h.notion.site/_IMC-c7b52b5e92574ea0a2e5ddb0c73d0a75',
     },
     {
       id: 4,
-      name: 'KMILK',
+      name: '공차',
       year: 2022,
       type: 'POPUP STORE',
-      img: other1,
+      img: other4,
+      url: 'https://sage-h.notion.site/IMC-2c073a849cde473798999099d6add7e6',
     },
     {
       id: 5,
-      name: 'LOCA',
+      name: '파크랜드',
       year: 2021,
       type: 'VIDEO',
-      img: other2,
-    },
-    {
-      id: 6,
-      name: '칠성제로',
-      year: 2022,
-      type: 'IMCPLAN',
-      img: other3,
+      img: other5,
+      url: 'https://sage-h.notion.site/_-981b3236a8a8432ab8b5f5263bc49fa3',
     },
   ];
   return (
-    <section className="section othersSection">
+    <section className='section othersSection'>
       {otherWorks.map((otherWork) => (
         <OthersFolder key={otherWork.id} data={otherWork} />
       ))}
