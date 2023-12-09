c065e467f6ce0953555824a4d9dd3d500520f753
jjsh03 <aberrant03@gmail.com>
2023-12-09 19:03:11 +0900

    design: others 반응형

--- src/components/OthersFolder/OthersFolder.js
+++ src/components/OthersFolder/OthersFolder.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './style.scss';
+import React from "react";
+import "./style.scss";
 
 export default function OthersFolder({ data }) {
   return (
     <div>
-      <a href={data.url} target='blank' style={{ color: 'white' }}>
+      <a href={data.url} target="blank" style={{ color: "white" }}>
         <div className={`otherFolder ${data.name}`}>
           <div className={`folderBack ${data.name}`}>
             <div />
             <div />
           </div>
-          <img src={data.img} alt='thumbnail' className='folderThumbnail'></img>
+          <img src={data.img} alt="thumbnail" className="folderThumbnail"></img>
           <div className={`folderFront ${data.name}front`}></div>
         </div>
-        <div className='folderInfo'>
-          {' '}
+        <div className="folderInfo">
+          {" "}
           <p>
             {data.name}_{data.year}
           </p>
--- src/components/WorkComponent/OthersCompo.js
+++ src/components/WorkComponent/OthersCompo.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import './OthersCompoStyle.scss';
-import OthersFolder from '../OthersFolder/OthersFolder';
-import other1 from '../../asset/others1.png';
-import other2 from '../../asset/others2.png';
-import other3 from '../../asset/others3.png';
-import other4 from '../../asset/others4.jpg';
-import other5 from '../../asset/others5.jpg';
+import React from "react";
+import "./OthersCompoStyle.scss";
+import OthersFolder from "../OthersFolder/OthersFolder";
+import other1 from "../../asset/others1.png";
+import other2 from "../../asset/others2.png";
+import other3 from "../../asset/others3.png";
+import other4 from "../../asset/others4.jpg";
+import other5 from "../../asset/others5.jpg";
 
 export default function OthersCompo() {
   const otherWorks = [
     {
       id: 1,
-      name: 'KMILK',
+      name: "KMILK",
       year: 2022,
-      type: 'POPUP STORE',
+      type: "POPUP STORE",
       img: other1,
-      url: 'https://sage-h.notion.site/_-ac404ad9611342c09f2adbcbd0a50191',
+      url: "https://sage-h.notion.site/_-ac404ad9611342c09f2adbcbd0a50191",
     },
     {
       id: 2,
-      name: 'LOCA',
+      name: "LOCA",
       year: 2021,
-      type: 'VIDEO',
+      type: "VIDEO",
       img: other2,
-      url: 'https://youtu.be/23kFUYgp20c',
+      url: "https://youtu.be/23kFUYgp20c",
     },
     {
       id: 3,
-      name: '칠성제로',
+      name: "칠성제로",
       year: 2021,
-      type: 'IMCPLAN',
+      type: "IMCPLAN",
       img: other3,
-      url: 'https://sage-h.notion.site/_IMC-c7b52b5e92574ea0a2e5ddb0c73d0a75',
+      url: "https://sage-h.notion.site/_IMC-c7b52b5e92574ea0a2e5ddb0c73d0a75",
     },
     {
       id: 4,
-      name: '공차',
+      name: "공차",
       year: 2022,
-      type: 'IMCPLAN',
+      type: "IMCPLAN",
       img: other4,
-      url: 'https://sage-h.notion.site/IMC-2c073a849cde473798999099d6add7e6',
+      url: "https://sage-h.notion.site/IMC-2c073a849cde473798999099d6add7e6",
     },
     {
       id: 5,
-      name: '파크랜드',
+      name: "파크랜드",
       year: 2021,
-      type: 'IMCPLAN',
+      type: "IMCPLAN",
       img: other5,
-      url: 'https://sage-h.notion.site/_-981b3236a8a8432ab8b5f5263bc49fa3',
+      url: "https://sage-h.notion.site/_-981b3236a8a8432ab8b5f5263bc49fa3",
     },
   ];
   return (
-    <section className='section othersSection'>
+    <section className="section othersSection">
       {otherWorks.map((otherWork) => (
         <OthersFolder key={otherWork.id} data={otherWork} />
       ))}
