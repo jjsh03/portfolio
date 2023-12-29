04d61a9b5224ed40c03f87882fcb65b2fe2ae99b
jjsh03 <139740067+jjsh03@users.noreply.github.com>
2023-12-29 17:42:56 +0900

    feat: 3차 프로젝트 목록 추가

--- src/components/WorkComponent/WebCompo.js
+++ src/components/WorkComponent/WebCompo.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "./WebCompoStyle.scss";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import './WebCompoStyle.scss';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function WebCompo() {
   useEffect(() => {
@@ -9,23 +9,23 @@ export default function WebCompo() {
       const mouseX = e.clientX;
       const mouseY = e.clientY;
       document.documentElement.style.setProperty(
-        "--mouseX",
+        '--mouseX',
         `${mouseX - 20}px`
       );
       document.documentElement.style.setProperty(
-        "--mouseY",
+        '--mouseY',
         `${mouseY - 20}px`
       );
     };
 
-    const webLists = document.querySelectorAll(".webList");
+    const webLists = document.querySelectorAll('.webList');
     webLists.forEach((webList) => {
-      webList.addEventListener("mousemove", handleMouseMove);
+      webList.addEventListener('mousemove', handleMouseMove);
     });
 
     return () => {
       webLists.forEach((webList) => {
-        webList.removeEventListener("mousemove", handleMouseMove);
+        webList.removeEventListener('mousemove', handleMouseMove);
       });
     };
   }, []);
@@ -37,8 +37,26 @@ export default function WebCompo() {
 
   return (
     <section className="section webSection">
-      <div className="webList" onClick={() => goToDetail(1)}>
+      <div className="webList" onClick={() => goToDetail(2)}>
         <h1>01</h1>
+        <h1>BEATBAY</h1>
+        <p className="listTeam">teamproject</p>
+        <p>
+          음악 관련 중고 거래와 재능 마켓을 접목한 음악 거래 웹 서비스 <br />
+          HTML, CSS, JavaScript, Sass, React, Axios, AOS, Figma, Git{' '}
+        </p>
+      </div>
+      <div className="webList" onClick={() => goToDetail(3)}>
+        <h1>02</h1>
+        <h1>AH-WHEW </h1>
+        <p className="listTeam">teamproject</p>
+        <p>
+          신세 한탄 AI 그림 일기 웹 서비스 <br />
+          HTML, SASS, React, Next.js TypeScript, Figma, Git{' '}
+        </p>
+      </div>
+      <div className="webList" onClick={() => goToDetail(1)}>
+        <h1>03</h1>
         <h1>MEALSAC</h1>
         <p className="listTeam">teamproject</p>
         <p>
@@ -48,31 +66,13 @@ export default function WebCompo() {
         </p>
       </div>
 
-      <div className="webList" onClick={() => goToDetail(2)}>
-        <h1>02</h1>
-        <h1>BEATBAY</h1>
-        <p className="listTeam">teamproject</p>
-        <p>
-          음악 관련 중고 거래와 재능 마켓을 접목한 음악 거래 웹 서비스 <br />
-          HTML, CSS, JavaScript, Sass, React, Axios, AOS, Figma, Git{" "}
-        </p>
-      </div>
-      <div className="webList" onClick={() => goToDetail(3)}>
-        <h1>03</h1>
-        <h1>SOMETHING</h1>
-        <p className="listTeam">teamproject</p>
-        <p>
-          세번째 팀플,, <br />
-          HTML, CSS, Javascript, ...
-        </p>
-      </div>
       <div className="webList" onClick={() => goToDetail(4)}>
         <h1>04</h1>
         <h1>PORTFOLIO</h1>
         <p className="listTeam">soloproject</p>
         <p>
           개인 포트폴리오 사이트(현재 사이트) <br />
-          HTML, CSS, JavaScript, Sass, React, AOS, Figma, Git{" "}
+          HTML, CSS, JavaScript, Sass, React, AOS, Figma, Git{' '}
         </p>
       </div>
     </section>
