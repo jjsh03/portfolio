d535a2fb9f304b15889803c0c9ecb9c9f452f9c6
jjsh03 <aberrant03@gmail.com>
2024-01-05 04:00:39 +0900

    feat: ahwhew figma 추가

--- src/components/WorkComponent/DesignCompo.js
+++ src/components/WorkComponent/DesignCompo.js
@@ -7,12 +7,14 @@ import artboard3 from '../../asset/artboard3.png';
 import artboard4 from '../../asset/poster1.png';
 import artboard5 from '../../asset/poster2.png';
 import artboard6 from '../../asset/poster3.png';
+import artboard7 from '../../asset/artboard7.png';
 import prevIcon from '../../asset/prev.svg';
 import nextIcon from '../../asset/next.svg';
 import Slider from 'react-slick';
 import modal1 from '../../asset/beatbay_figma.png';
 import modal2 from '../../asset/mealsac_figma.png';
 import modal3 from '../../asset/portfolio_figma.png';
+import modal4 from '../../asset/ahwhew_figma.png';
 import { HashLoader } from 'react-spinners';
 
 export default function DesignCompo() {
@@ -26,27 +28,34 @@ export default function DesignCompo() {
     },
     {
       id: 2,
+      name: '아휴',
+      type: 'FIGMA',
+      img: artboard7,
+      modal: modal4,
+    },
+    {
+      id: 3,
       name: '밀싹',
       type: 'FIGMA',
       img: artboard2,
       modal: modal2,
     },
     {
-      id: 3,
+      id: 4,
       name: '포트폴리오',
       type: 'FIGMA',
       img: artboard3,
       modal: modal3,
     },
     {
-      id: 4,
+      id: 5,
       name: '틈',
       type: 'POSTER',
       img: artboard4,
       modal: artboard4,
     },
     {
-      id: 5,
+      id: 6,
       name: 'FASHION',
       type: 'POSTER',
       img: artboard5,
